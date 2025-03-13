f374bdc106be34459a594063be8bb5b53db27c34
Desai Yash Manish <yashmdesai20@gnu.ac.in>
2025-03-13 14:47:00 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// This file is intentionally left mostly empty because all JavaScript 
+// functionality is currently included directly in the HTML file
+console.log("External JavaScript file loaded successfully");
+
+// You could move the JavaScript from your HTML file to here if desired
+// for better code organization
