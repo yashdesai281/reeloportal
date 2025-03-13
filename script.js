290a2ef5e5cdf3ac9cce744049cd4148f6910966
Desai Yash Manish <yashmdesai20@gnu.ac.in>
2025-03-13 14:51:00 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// This file is intentionally left mostly empty because all JavaScript 
-// functionality is currently included directly in the HTML file
-console.log("External JavaScript file loaded successfully");
-
-// You could move the JavaScript from your HTML file to here if desired
-// for better code organization
